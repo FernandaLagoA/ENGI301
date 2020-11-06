3034243d79e16f25d22bc403286593bf0c83dde7
FernandaLagoA <71305249+FernandaLagoA@users.noreply.github.com>
2020-11-06 11:41:13 -0600

    Create project1.sh

---
+++ project_01/project1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Connect to the wifi
+# connmanctl enable wifi
+# connmanctl agent on
+# connmanctl connect wifi_74da38de508b_526963652056697369746f72_managed_none
+
+#set up symbolic link
+
+echo temppwd | sudo -S ln -s /dev/spidev1.0 /dev/spidev0.0
+
+
+# Change to appropriate directory
+cd /var/lib/cloud9/ENGI301/Project
+
+# Runs the code to display
+
+PYTHONPATH=/var/lib/cloud9/ENGI301/Project python3 main.py
